b074d4dd707d191a01f4aee77075b05ba8e3e13f
Abel Cerezuela <abel.cerezuela@gft.com>
2023-04-21 12:28:31 +0200

    improve styles

--- projects/00-hola-mundo/src/TwitterFollowCard.jsx
+++ projects/00-hola-mundo/src/TwitterFollowCard.jsx
@@ -21,7 +21,8 @@ export function TwitterFollowCard({ children, userName = "unknown", initialIsFol
       </header>
       <aside>
         <button className={buttonClassName} onClick={handleClick}>
-          {text}
+          <span className="tw-followCard-text">{text}</span>
+          <span className="tw-followCard-stopFollow">Dejar de seguir</span>
         </button>
       </aside>
     </article>
